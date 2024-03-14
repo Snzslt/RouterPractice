06e4b20ea2cdbb074bfdf8f46bf639230513e70e
Sanaz Soltani <74157401+Snzslt@users.noreply.github.com>
2024-03-14 10:56:54 -0400

    Deffering Data Fetching with defer()

--- frontend/src/pages/EventsPage.js
+++ frontend/src/pages/EventsPage.js
@@ -1,43 +1,61 @@
-import { json } from "react-router-dom";
-import { useLoaderData } from "react-router-dom";
+import { json, defer, useLoaderData, Await } from "react-router-dom";
+import { Suspense } from "react";
+
 
 import EventsList from "../components/EventsList";
 
 function EventsPage() {
-  const events = useLoaderData();
+  const {events} = useLoaderData();
+
+   return(
+     <Suspense fallback={<p style={{textAlign: 'center'}}>Loading...</p>}>
+     <Await resolve={events}>
+     {(loadedEvents) => <EventsList events={loadedEvents}/>}
+     </Await>
+     </Suspense>
+     )
+//const events = data.events;
+  
 //   if (data.isError) {
 //     return <p>{data.message}</p>;
 //   }
- //
-  return (
-    <>
-      <EventsList events={events} />
-    </>
-  );
+//
+//return (
+//  <>
+//   <EventsList events={events} />
+// </>
+//);
 }
 
 export default EventsPage;
-export async function loader() {
-  //we can not use react hooks in loaders
-  const response = await fetch("http://localhost:8080/events");
 
-  if (!response.ok) {
-    //setError("Fetching events failed.");
-    //return { isError: true, message: "Could not fetch events." };
-    //throw  { message: 'Could not fetch events'};
-    //throw new Response(JSON. stringify({message: 'Could not fetch events.'}),
-    //{
-        //status: 500,
-    //});
-    return json(
-        {message: 'Could not fetch events.'},
-        {
-            status:500,
-        }
-    );
-  } else {
-    const resData = await response.json();
-    return resData.events;
-    //return resposne;
-  }
+async function loadEvents(){
+    //we can not use react hooks in loaders
+    const response = await fetch("http://localhost:8080/events");
+
+    if (!response.ok) {
+      //setError("Fetching events failed.");
+      //return { isError: true, message: "Could not fetch events." };
+      //throw  { message: 'Could not fetch events'};
+      //throw new Response(JSON. stringify({message: 'Could not fetch events.'}),
+      //{
+          //status: 500,
+      //});
+      return json(
+          {message: 'Could not fetch events.'},
+          {
+              status:500,
+          }
+      );
+    } else {
+      const resData = await response.json();
+      return resData.events;
+      //return resposne;
+      
+    }
+}
+export function loader() {
+ return defer({
+   events: loadEvents(),
+ })
 }
